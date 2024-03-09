f29bbd2af663f457e21c439ca0da1d7469707329
Anna <anna.berejna@gmail.com>
2024-03-09 19:47:42 +0200

    upd

--- utils/tender-data.ts
+++ utils/tender-data.ts
@@ -3,45 +3,35 @@ import {
   generateRandomWord,
   generateRandomWords,
   generateRandomSymbols,
-  generateRandomLetters,
+  generateRandomCyrillicLetters,
   generateRandomStringDigits,
   generateRandomTextLines,
   generateRandomCity,
-} from "../utils/random.util";
+} from "./random.util";
 
 const randomName = "Тестовий Тендер " + generateRandomCombination();
-const randomServiceName = generateRandomLetters(1);
+const randomServiceName = generateRandomCyrillicLetters(1);
 const newRandomServiceName = "Тестова Послуга " + generateRandomCombination();
 
 export const validTenderData: { [key: string]: string } = {
   name: randomName,
   serviceName: randomServiceName,
   newServiceName: newRandomServiceName,
-  workAddress: generateRandomLetters(1),
+  workAddress: generateRandomCyrillicLetters(1),
   budgetAmount: generateRandomStringDigits(8),
   additionalInfo: generateRandomTextLines(6),
 };
 
-const invalidName = [
-  " ",
-  generateRandomWords(25),
-  generateRandomWord(1, 9),
-];
-const invalidServiceName = [" ", generateRandomWords(50)];
-const invalidBudgetData = [
-  generateRandomSymbols(5),
-  generateRandomLetters(5),
-  " ",
-  "0",
-  generateRandomStringDigits(10),
-];
-const invalidPlaceData = [generateRandomCity()];
-const invalidAdditionalInfo = [" ", generateRandomWords(1)];
-
 export const invalidTenderData: { [key: string]: string[] } = {
-  name: invalidName,
-  serviceName: invalidServiceName,
-  workAddress: invalidPlaceData,
-  budgetAmount: invalidBudgetData,
-  additionalInfo: invalidAdditionalInfo,
+  name: [" ", generateRandomWords(25), generateRandomWord(1, 9)],
+  serviceName: [" ", generateRandomWords(50)],
+  workAddress: [generateRandomCity()],
+  budgetAmount: [
+    generateRandomSymbols(5),
+    generateRandomCyrillicLetters(5),
+    " ",
+    "0",
+    generateRandomStringDigits(10),
+  ],
+  additionalInfo: [" ", generateRandomWords(1)],
 };
